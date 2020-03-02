1e26e6bae1a982ef7e03cacb59b9a1053462a703
Prabhakar Singh <49479445+pksingh007@users.noreply.github.com>
2020-03-03 01:15:30 +0530

    My code
    Added header , content and footer section in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,103 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <div id="app">
+    <v-app>
+      <!-- Header section start -->
+      <v-app-bar app color="primary" dark>
+        
+        <v-toolbar-title>
+          <span class="title ml-3 mr-5">Len<span class="font-weight-light">Skills</span></span>
+        </v-toolbar-title>
+        
+        <!-- <v-spacer></v-spacer>
+        <v-btn text rounded>Home</v-btn>
+        <v-btn text rounded>Login</v-btn> -->
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      </v-app-bar>
+      <!-- Header section stop -->
 
-      <v-spacer></v-spacer>
+      <!-- Content section start -->
+      <v-content>
+        <!-- Create Post section start -->
+        <v-card width="600" class="mx-auto mt-10">
+      
+            <v-card-title class="pb-2">
+            <!-- <img src="https://picsum.photos/510/300?random" width="35" height="35"> -->
+            <h6>Create post</h6>
+            </v-card-title>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+            <v-divider></v-divider>
+            <v-form>
+                    
+                    <v-row align="center">
+                      <v-col class="text-center" cols="12" sm="12"> 
+                        
+                        <v-text-field
+                          type="text"
+                          placeholder="What's on your mind?"
+                          prepend-icon="mdi-account-circle"
+                          
+                        ></v-text-field>
+                        
+                        <v-file-input
+                        :rules="rules"
+                        accept="image/png, image/jpeg, image/bmp"
+                        placeholder="Upload Photo/Video"
+                        ></v-file-input>
+                        
+                        <v-spacer></v-spacer>
+                        <v-divider></v-divider>
+                        <br/>
+                        <v-btn small color="info" v-on:click="readRefs">Post</v-btn>
+                      </v-col>
+                    </v-row>
+            </v-form>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+        </v-card>
+        <!-- Create Post section stop -->
+
+        <!-- Show Post section start -->
+        <v-card width="600" class="mx-auto mt-10">
+          <v-card-title class="pb-2">
+            <img src="https://picsum.photos/510/300?random" width="35" height="35">
+            <h6 class="ml-2"> Karan Yadav</h6>
+            </v-card-title>
+          <div class="ml-5">My First express when i become father rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr</div>
+          <v-img src="https://picsum.photos/510/300?random" aspect-ratio="2"></v-img>
+        </v-card>
+        <!-- Show Post section stop -->
+
+      </v-content>
+      <!-- Content section stop -->
+
+      <br/>
+
+      <!-- Footer section start-->
+      <v-footer color="primary lighten-1" padless>
+        <v-layout justify-center wrap>
+          <v-flex primary lighten-2 py-4 text-center white--text xs12>
+            Copyright@{{ new Date().getFullYear() }} — <strong>LenSkills.com</strong>
+          </v-flex>
+        </v-layout>
+      </v-footer>
+      <!-- Footer section stop-->
+
+    </v-app>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
 
+<script>
 export default {
   name: 'App',
+  data () {
+    return {
 
-  components: {
-    HelloWorld,
+    }
   },
-
-  data: () => ({
-    //
-  }),
-};
+  methods:{
+    readRefs:function(){
+      console.log(this.$refs.input.text);
+    }
+  }
+}
 </script>
